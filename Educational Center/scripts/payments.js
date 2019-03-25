10b8478ba9aafa7dbaa1b975fc326b0747a43b91
Vas <vas0977515005@gmail.com>
2019-03-25 17:06:03 +0200

    Home Work

---
+++ Educational Center/scripts/payments.js
@@ -0,0 +1,22 @@
+var paymentOneMonth = 3000,
+    numberOfMonth = 3,
+    certificate = true,
+    discount = 0;
+var totalPayment = paymentOneMonth*numberOfMonth;
+console.log('Payment for course:', totalPayment, 'uah.'); 
+
+   if (totalPayment >= 9000){
+	   discount = 10;
+   }
+      console.log('Discount for whole course:', discount + '%');
+	
+   if (certificate){
+	discount += 5
+   console.log('Additional certificate discount 5%')	
+  }
+  totalPayment -= totalPayment*discount/100;  
+  console.log('Payment with discount:', totalPayment, 'uah.'); 
+   alert(totalPayment);
+  
+   
+ 
---
+++ Nova Poshta/scripts/package.js
@@ -0,0 +1,37 @@
+var numberOfCode = 1111,
+passport = false,
+smsCode = true;
+
+    var someCode = 1111;
+  
+  if (numberOfCode == someCode)
+  
+  {
+  console.log('Your number is right! Give me your passport or sms code from phone.');
+  }
+  else 
+  {
+	  console.log('Your number is wrong! Package stay on the Nova poshta.');
+  }
+   
+  if (passport){
+	console.log('My name and lastname from passport are Vasyl Martynenko.');
+	    }
+	else{
+	console.log('If do not have passport use sms from phone.');
+	}	
+    
+  if (smsCode){
+	console.log('Sms code on the phone is 18632.');
+	    }
+	else{
+	console.log('If do not have phone use your passport.');
+	}	
+	
+	if (numberOfCode == someCode ) {
+	console.log ('You can get your package from Nova Poshta.')
+	}
+	else{
+		console.log ('You can not get your package from Nova Poshta.')
+		}
+	
